24f2137f4677d851547c9634dbd8cc7dd0d72d68
shonjmoj <midoounejjart@gmail.com>
2024-10-21 16:05:52 +0100

    feat: add create, uodate and delete actions

---
+++ src/actions/createTodo.ts
@@ -0,0 +1,22 @@
+import { revalidatePath } from "next/cache";
+
+import { prisma } from "@/lib/prisma";
+
+export const createTodo = async (formData: FormData) => {
+  "use server";
+
+  const title = formData.get("title") as string;
+
+  try {
+    if (title) {
+      await prisma.todo.create({
+        data: {
+          title,
+        },
+      });
+    }
+    return revalidatePath("/");
+  } catch {
+    return;
+  }
+};
